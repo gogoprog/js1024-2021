af0c40ca2c9a14a3bd073d2d63cf69d78920ecc6
Gauthier Billot <gogoprog@gmail.com>
2021-06-30 22:59:28 +0200

    wip

--- src/Main.hx
+++ src/Main.hx
@@ -10,10 +10,18 @@ class Main {
         Shim.canvas.width = Shim.canvas.height = screenSize;
         var time:Int = 0;
         var pixels:Array<Int> = [];
-        var colors = [ '#000', '#030', '#111', '#240', '#350', '#600', '#650', '#700', '#750', '#800', '#999', '#aaa', '#baa', '#bbb', '#caa', '#daa', '#faa'];
+        var colors = [ '#000', '#030', '#111', '#240', '#350', '#400', '#450', '#640', '#650', '#700', '#750', '#800', '#999', '#aaa', '#baa', '#bbb', '#caa', '#daa', '#dda', '#faa'];
         var w = 256;
         var h = 32;
-        function scale(s) {
+        var mx:Int;
+        var bx;
+        var by = 190;
+        var vx = 1;
+        var vy = -1;
+        var py = 200;
+        var hits:Array<Int> = [];
+        var stick = true;
+        inline function scale(s) {
             Shim.context.scale(s, s);
         }
         function col(n:Dynamic) {
@@ -28,6 +36,13 @@ class Main {
         inline function drawPixel(x:Int, y:Int, c:Int) {
             col(colors[c]);
             Shim.context.fillRect(x, y, 1, 1);
+        }
+        untyped onmousemove = function(e) {
+            mx = e.clientX / 2;
+
+            if(e.buttons) {
+                stick = false;
+            }
         }
         function drawCircle(x, y, r) {
             Shim.context.beginPath();
@@ -49,13 +64,55 @@ class Main {
                 setPixel(x - r, y - 1, p - (r == 3 ? 0 : 1));
             }
         }
+        function collision(x, y, i, j) {
+            Shim.context.fillRect(x, y, i, j);
+
+            if(bx >= x && bx <= x + i) {
+                if(by == y || by == y + j) { vy *= -1; return true;}
+            }
+
+            if(by >= y && by <= y + j) {
+                if(bx == x || bx == x + i) { vx *= -1; return true;}
+            }
+
+            return false;
+        }
         function loop(t:Float) {
             {
-                Shim.canvas.width = w*2;
                 scale(2);
-
                 col('#000');
                 Shim.context.fillRect(0, 0, w, w);
+                col('#a44');
+
+                if(stick) {
+                    bx = mx;
+                } else {
+                    bx += vx;
+                    by += vy;
+
+                    if(by > py +10) {
+                        stick = true;
+                        by = 190;
+                        vy = -1;
+                    }
+                }
+
+                for(j in 0...10) {
+                    for(i in 0...13) {
+                        if(untyped !hits[i * w + j]) {
+                            if(collision(14 + i * 18, 15 + j * 10, 16, 4)) {
+                                hits[i * w + j] = 1;
+                            }
+                        }
+                    }
+                }
+
+                col('#888');
+                collision(mx-7, 200, 20, 4);
+                collision(0, 0, w, 4);
+                collision(0, 0, 4, py);
+                collision(w-4, 0, 4, py);
+                Shim.context.fillRect(bx, by, 1, 1);
 
                 for(y in 0...h) {
                     for(x in 0...w) {
@@ -63,15 +120,17 @@ class Main {
                         drawPixel(x, w - h + y, pixels[y * w + x]);
                     }
                 }
+
+                scale(0.5);
             }
-            // untyped requestAnimationFrame(loop);
-            untyped setTimeout(loop, 30);
+            // untyped setTimeout(loop, 10);
+            untyped requestAnimationFrame(loop);
         }
 
         for(y in 0...h) {
             for(x in 0...w) {
                 setPixel(x, y, 0);
-                setPixel(x, h - 1, 16);
+                setPixel(x, h - 1, 18);
             }
         }
 
